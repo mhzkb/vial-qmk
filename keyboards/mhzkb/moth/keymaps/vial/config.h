0956b372306f5a9a2a8aab6abafb0077eca798ae
mhzkb <kb@haugezeuner.dk>
2025-10-27 10:39:05 +0100

    Added port for Moth

---
+++ keyboards/mhzkb/moth/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEC, 0x9D, 0x22, 0x59, 0xE2, 0x22, 0x1B, 0x9E}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+
